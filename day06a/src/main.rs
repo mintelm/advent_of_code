f15c453c98fbe752b98e02b9a0121b81efe9c675
Mario Mintel <mariomintel@gmail.com>
2021-12-12 17:45:42 +0100

    optimize solution day06a

--- day06a/src/main.rs
+++ day06a/src/main.rs
@@ -1,21 +1,19 @@
 const DAYS: usize = 80;
 
 fn main() {
-    let mut lanternfish: Vec<u8> = include_str!("../input.txt")
-        .split(',')
-        .map(|s| s.parse().unwrap())
-        .collect();
+    let mut lanternfish: [u128; 9] = [0; 9];
 
-    for _ in 0..DAYS {
-        for i in 0..lanternfish.len() {
-            if lanternfish[i] != 0 {
-                lanternfish[i] -= 1;
-            } else {
-                lanternfish[i] = 6;
-                lanternfish.push(8);
-            }
-        }
+    include_str!("../input.txt")
+        .trim()
+        .split(',')
+        .for_each(|s| {
+            let i: usize = s.parse().unwrap();
+            lanternfish[i] += 1;
+        });
+
+    for day in 1..DAYS {
+        lanternfish[(day + 7) % 9] += lanternfish[day % 9];
     }
 
-    println!("{:?}", lanternfish.len());
+    println!("{:?}", lanternfish.iter().sum::<u128>());
 }
