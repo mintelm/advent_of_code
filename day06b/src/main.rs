fe4e68dc1fe131ca63e81379c93090c5a0a3e67d
Mario Mintel <mariomintel@gmail.com>
2021-12-12 17:44:50 +0100

    add solution for day06b

---
+++ day06b/src/main.rs
@@ -0,0 +1,19 @@
+const DAYS: usize = 256;
+
+fn main() {
+    let mut lanternfish: [u128; 9] = [0; 9];
+
+    include_str!("../input.txt")
+        .trim()
+        .split(',')
+        .for_each(|s| {
+            let i: usize = s.parse().unwrap();
+            lanternfish[i] += 1;
+        });
+
+    for day in 1..DAYS {
+        lanternfish[(day + 7) % 9] += lanternfish[day % 9];
+    }
+
+    println!("{:?}", lanternfish.iter().sum::<u128>());
+}
